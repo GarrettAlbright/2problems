2e0cb0f5908dd175f2a9fce307ccf4fefc766f24
Garrett Albright <albright@abweb.us>
2013-02-22 01:06:17 +0900

    Hey, we can compile patterns and get results! Cool.

--- 2p.js
+++ 2p.js
@@ -8,6 +8,35 @@ $(document).ready(function() {
         'pattern': '',
         'testText': '',
       };
+    },
+    // Store the compiled pattern.
+    'compiledPattern': null,
+    // Store matches.
+    'matches': new Array,
+    // Parse the pattern string and compile the pattern. Returns true or false
+    // based on success of the new pattern compilation.
+    'compilePattern': function(pattern) {
+      // The first character must be the pattern delimiter.
+      var patternDelimiter = pattern.charAt(0),
+        // Build a pattern to extract the pattern and its flags.
+        patternPattern = new RegExp('\\' + patternDelimiter + '(.+)\\' + patternDelimiter + '(.*)'),
+        patternParts = pattern.match(patternPattern);
+      // If we're building a new pattern, we want to flush the old match
+      // results.
+      this.matches = new Array;
+      try {
+        this.compiledPattern = new RegExp(patternParts[1], patternParts[2]);
+      }
+      catch (e) {
+        this.compiledPattern = null;
+        return false;
+      }
+      return true;
+    },
+    // Run our compiled pattern against text and store the results.
+    'executePattern': function(haystack) {
+      this.matches = haystack.match(this.compiledPattern);
+      console.log(this.matches);
     }
   });
   
@@ -26,9 +55,23 @@ $(document).ready(function() {
   var TesterView = Backbone.View.extend({
     'tagName': 'article',
     'template': _.template($('#tester-t').html()),
+    'events': {
+      'keyup .pattern': 'sendPattern',
+      'keyup .haystack': 'sendHaystack'
+    },
     'render': function() {
       this.$el.html(this.template(this.model.toJSON()));
       return this;
+    },
+    'sendPattern': function() {
+      var patternText = $('.pattern', this.$el).val();
+      if (patternText && this.model.compilePattern(patternText)) {
+        this.sendHaystack();
+      }
+    },
+    'sendHaystack': function() {
+      this.model.executePattern($('.haystack', this.$el).val());
+      // Get results and build list here.
     }
   });
 
